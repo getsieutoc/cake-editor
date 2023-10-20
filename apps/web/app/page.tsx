41f503ecf561f12ec72f42b4b889ffbc6ad216ec
Sang Dang <sangdth@gmail.com>
2023-10-20 21:52:49 +0300

    change the rest of names

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+import { Editor } from "@sieutoc/cake-editor";
+
+export default function Page() {
+  return (
+    <>
+      <h1>Editor</h1>
+
+      <Editor />
+    </>
+  );
+}
---
+++ packages/core/src/Editor.tsx
@@ -0,0 +1,9 @@
+export interface EditorProps {
+  title?: string;
+}
+
+export function Editor(props: EditorProps) {
+  return <div>{props.title ?? "Cake Editor"}</div>;
+}
+
+Editor.displayName = "Editor";
